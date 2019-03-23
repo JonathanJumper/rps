ac53b6e43cf9175bd9630dfe5b220c80cb7bcf9a
Jumper <jomartinezch@gmail.com>
2019-03-22 20:25:25 -0500

    My initial commit;

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/logo.png';
+
 
 class App extends Component {
+
+  state = {
+    player1: '',
+    player2: '',
+
+  };
+
   render() {
     return (
       <div className="App">
+
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          ¿¡Piedra, Papel o Tijera!?
         </header>
+
+        <div className="App-container">
+          <button>
+            Nuevo Juego
+          </button>
+          <button>
+            Histórico
+          </button>
+        </div>
+
+        <div>
+          <img src={logo} className="logo"/>
+        </div>
+
       </div>
     );
   }
